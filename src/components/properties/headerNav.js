3b4deb4664bdac63d067f6cb5eaca010a94ce28b
shubhamZangula <shubham@zangula.com>
2024-09-18 21:31:26 +0530

    worded on top header

--- src/components/properties/headerNav.js
+++ src/components/properties/headerNav.js
@@ -22,6 +22,38 @@ const HeaderNav = () => {
     return(
         <>
             <div className="fixed w-screen top-0 z-50">
+                    <div className="bg-[#765DFF] flex items-center">
+                        <div className="px-3 md:px-5 py-1.5 flex justify-between w-full">
+                            <div className="flex justify-between w-full md:w-fit gap-8 font-semibold text-white text-sm">
+                                <p className="hover:text-red-100">
+                                    <a href="tel:+917708545098">
+                                        +91 77085 45098
+                                    </a>
+                                </p>
+                                <p className="hover:text-red-100">
+                                    <a href="mailto:er.riyaz2507@gmail.com?subject=Hello&body=I%20want%20to%20ask%20about..." target="_blanck">
+                                        er.riyaz2507@gmail.com
+                                    </a>
+                                </p>
+                            </div>
+                            <div className="self-center flex gap-8 hidden md:inline-flex">
+                                <div className="size-7">
+                                    <a href='http://www.linkedin.com/in/riyaz-ahamed-18638317a'>
+                                        <svg className="fill-white hoverfill-blue-800" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50">    
+                                            <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div className="size-7">
+                                    <a href="https://wa.me/+917708545098" target="_blanck">
+                                        <svg className="fill-white hoverfill-green-500" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50">    
+                                            <path d="M25,2C12.318,2,2,12.318,2,25c0,3.96,1.023,7.854,2.963,11.29L2.037,46.73c-0.096,0.343-0.003,0.711,0.245,0.966 C2.473,47.893,2.733,48,3,48c0.08,0,0.161-0.01,0.24-0.029l10.896-2.699C17.463,47.058,21.21,48,25,48c12.682,0,23-10.318,23-23 S37.682,2,25,2z M36.57,33.116c-0.492,1.362-2.852,2.605-3.986,2.772c-1.018,0.149-2.306,0.213-3.72-0.231 c-0.857-0.27-1.957-0.628-3.366-1.229c-5.923-2.526-9.791-8.415-10.087-8.804C15.116,25.235,13,22.463,13,19.594 s1.525-4.28,2.067-4.864c0.542-0.584,1.181-0.73,1.575-0.73s0.787,0.005,1.132,0.021c0.363,0.018,0.85-0.137,1.329,1.001 c0.492,1.168,1.673,4.037,1.819,4.33c0.148,0.292,0.246,0.633,0.05,1.022c-0.196,0.389-0.294,0.632-0.59,0.973 s-0.62,0.76-0.886,1.022c-0.296,0.291-0.603,0.606-0.259,1.19c0.344,0.584,1.529,2.493,3.285,4.039 c2.255,1.986,4.158,2.602,4.748,2.894c0.59,0.292,0.935,0.243,1.279-0.146c0.344-0.39,1.476-1.703,1.869-2.286 s0.787-0.487,1.329-0.292c0.542,0.194,3.445,1.604,4.035,1.896c0.59,0.292,0.984,0.438,1.132,0.681 C37.062,30.587,37.062,31.755,36.57,33.116z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 <div className="bg-white flex justify-between h-16 px-8 py-2 dark:bg-black">
                     <div className="flex gap-3">
                         {/* <img src={IMAGE.logo} alt="logo"/> */}
@@ -79,7 +111,7 @@ const HeaderNav = () => {
                             </div>
                         </div> 
                         <div className="inline-block">
-                            <div className={`fixed flex flex-col justify-between pb-20 top-16 right-48 md:hidden z-40 text-black dark:text-white h-full pt-3 overflow-y-auto transition-transform translate-x-full  dark:bg-transparent backdrop-blur-md w-48 ${showMenu === true ? null : 'hidden'}`}>
+                            <div className={`fixed flex flex-col justify-between pb-20 top-[95px] right-48 md:hidden z-40 text-black dark:text-white h-full pt-3 overflow-y-auto transition-transform translate-x-full  dark:bg-transparent backdrop-blur-md w-48 ${showMenu === true ? null : 'hidden'}`}>
                                 <ul className="*:bg-re *:my-1 *:p-2 *:text-center *:flex *:items-center *:justify-end ">
                                     <a  onClick={()=>setShowMenu(!showMenu)} href={'#Home'} className="hover:bg-gray-100 hover:text-black group hover:font-semibold *:mx-2" style={{fontWeight : Router.pathname === '/' ? 'bold': null,background:Router.pathname==='/'?'#f3f4f6':null,}}>
                                         <p className="">Home</p>  
